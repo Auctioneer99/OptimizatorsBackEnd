0d6480b180c6711bcfaa6ea21084682de8ac5d03
Auctioneer99 <42092940+Auctioneer99@users.noreply.github.com>
2021-06-14 13:46:20 +0500

    2

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cookieParser());
 app.use(authorizer);
 
