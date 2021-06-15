8ee7201f7309c6d96dafc4776ef66074d7861dfa
aldebaranoro <anatol.karas@gmail.com>
2021-06-15 18:53:40 +0500

    cookie fix

--- middleware/JWTAuthorizer.js
+++ middleware/JWTAuthorizer.js
@@ -2,8 +2,11 @@ import "core-js";
 import "regenerator-runtime";
 
 import auth from "../libs/Auth.js";
+import credentials from "../credentials";
 
-function cookieAuthorize(req, res, next) {
+const { ACCESS_TOKEN_ALIVE } = credentials.auth;
+
+async function cookieAuthorize(req, res, next) {
   let authToken = req.cookies.Authorization;
   let refreshToken = req.cookies.Refresh;
 
@@ -14,72 +17,58 @@ function cookieAuthorize(req, res, next) {
     return;
   }
 
+  let updated = false;
+
   if (authToken && refreshToken) {
-    verifyAccessToken(req, res).then(
-      (_) => {
-        next();
-        return;
-      },
-      (err) => {
-        next();
-        return;
-      }
-    );
-  } else {
+    try{
+      await verifyAccessToken(req, res)
+      updated = true;
+      next();
+      return;
+    }
+    catch (ex)
+    {
+      console.log("Error when verifying access token")
+      console.log(ex)
+    }
+  } 
+  
+  if (updated == false)
+  {
     if (refreshToken) {
-      updateAccessToken(req, res).then(
-        (_) => {
-          cookieAuthorize(req, res, next);
-          return;
-        },
-        (err) => {
-          next();
-          return;
-        }
-      );
+      await updateAccessToken(req, res);
+      cookieAuthorize(req, res, next);
     }
   }
 }
 
-function updateAccessToken(req, res) {
-  let promise = new Promise(function (resolve, reject) {
-    let refreshToken = req.cookies.Refresh;
-    auth.refreshAccessToken(refreshToken).then(
-      (token) => {
-        res.cookie("Authorization", token, {
-          maxAge: REFRESH_TOKEN_ALIVE,
-          httpOnly: false,
-          secure: true,
-          sameSite: "none",
-        });
-        resolve(token);
-      },
-      (err) => {
-        res.clearCookie("Authorization").clearCookie("Refresh");
-        console.log(err.message);
-        reject(err);
-      }
-    );
-  });
-  return promise;
+async function updateAccessToken(req, res) {
+
+  let refreshToken = req.cookies.Refresh;
+  try
+  {
+    let token = await auth.refreshAccessToken(refreshToken);
+
+    res.cookie("Authorization", token, {
+      maxAge: ACCESS_TOKEN_ALIVE,
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+    });
+  }
+  catch (ex)
+  {
+    console.log("Execption when refreshing access token")
+    res.clearCookie("Authorization").clearCookie("Refresh");
+    console.log(ex);
+  }
 }
 
-function verifyAccessToken(req, res, next) {
-  let promise = new Promise(function (resolve, reject) {
-    let authToken = req.cookies.Authorization;
-    auth.verifyAccessToken(authToken).then(
-      (payload) => {
-        req.user = payload;
-        resolve(payload);
-      },
-      (err) => {
-        res.clearCookie("Authorization").clearCookie("Refresh");
-        console.log(err.message);
-        reject(err);
-      }
-    );
-  });
-  return promise;
+async function verifyAccessToken(req, res) {
+  
+  let authToken = req.cookies.Authorization;
+  let payload = await auth.verifyAccessToken(authToken);
+  req.user = payload;
 }
 
 export default cookieAuthorize;
